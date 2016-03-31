6ce8e9adde620d2a6a24c2d60d97061194f5eb49
Ben Calabrese <bencalabrese@gmail.com>
2016-03-31 11:49:16 -0700

    created sessions and basic auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      redirect_to user_url(@user)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
   end
 
   private
