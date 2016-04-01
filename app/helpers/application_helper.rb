eeef948fa5b670ab895d02e37f797acee6952338
Ben Calabrese <bencalabrese@gmail.com>
2016-03-31 17:33:19 -0700

    added ugly lyrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def ugly_lyrics(lyrics)
+    lines = lyrics.split("\n")
+
+    html = "<pre>"
+    lines.each do |line|
+      html += "&#x2603 " + h(line)
+    end
+    html += "</pre>"
+
+    html.html_safe
+  end
 end
